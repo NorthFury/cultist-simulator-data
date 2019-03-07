ced98428f100f79143d3635934001640b91660c9
NorthFury <robyter@gmail.com>
2019-03-07 22:38:48 +0100

    rewrote readResource to properly sanitize the JSON

--- src/util/readResource.js
+++ src/util/readResource.js
@@ -2,15 +2,64 @@ const fs = require("fs")
 const path = require('path')
 const { gamePath } = require("../../config.json")
 
-const assetsPath = path.join(gamePath, "cultistsimulator_Data/StreamingAssets/content/core")
-
 function readResource(file) {
-    var content = fs.readFileSync(path.join(assetsPath, file), "utf-8")
-    if (content.endsWith(";")) content = content.substr(0, content.length - 1)
-    content = content.replace(/([{[,])[\s\r\n]*([a-zA-Z][\w.]+)\s*:/g, '$1"$2":')
-    content = content.replace(/":[\s\r\n]*([a-zA-Z][\w.]+)/g, '":"$1"')
-    content = content.replace(/,[\s\r\n]*(]|})/g, "$1")
-    return JSON.parse(content)
+    const fullPath = path.join(gamePath, "cultistsimulator_Data/StreamingAssets/content/core", file)
+    const content = fs.readFileSync(fullPath, "utf-8")
+
+    const digitPattern = /\d/
+    const keyPattern = /[\w.]/
+
+    const sb = []
+    let i = 0
+    while (i < content.length) {
+        let c = content[i++]
+        //drop white space characters
+        if (c === ' ' || c === '\t' || c === '\r' || c === '\n') {
+            continue
+        }
+        // quoted token
+        if (c === '"') {
+            let index = i
+            do {
+                c = content[index++]
+            } while (c !== '"' || (c === '"' && content[index - 2] === '\\'))
+            sb.push(content.substring(i - 1, index).replace(/[\r\n]+/g, ''))
+            i = index
+            continue
+        }
+        // number token
+        if (digitPattern.test(c)) {
+            let index = i
+            do {
+                c = content[index++]
+            } while (digitPattern.test(c))
+            sb.push(content.substring(i - 1, index - 1))
+            i = index - 1
+            continue
+        }
+        // token that should be quoted
+        if (keyPattern.test(c)) {
+            let index = i
+            do {
+                c = content[index++]
+            } while (keyPattern.test(c))
+            sb.push('"')
+            sb.push(content.substring(i - 1, index - 1))
+            sb.push('"')
+            i = index - 1
+            continue
+        }
+        // remove trailing comma
+        if (sb.length > 0 && sb[sb.length - 1] === ',' && (c === ']' || c === '}')) {
+            sb.pop()
+        }
+        sb.push(c)
+    }
+
+    // drop trailing semicolon
+    if (sb[sb.length - 1] === ";") sb.pop()
+
+    return JSON.parse(sb.join(""))
 }
 
 module.exports = readResource
