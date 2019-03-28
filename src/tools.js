1ac87ce09b3f6a576bab164a751fb015be181c96
NorthFury <robyter@gmail.com>
2019-03-28 19:39:04 +0100

    added gift data to tools parser

--- src/tools.js
+++ src/tools.js
@@ -1,11 +1,38 @@
 const toLuaTable = require("./util/toLuaTable")
 const readResource = require("./util/readResource")
 
+const toolGifts = readResource("/recipes/talking_8_follower.json").recipes
+    .filter(it => it.id.startsWith("L_give_tool_") && it.requirements)
+    .reduce((acc, it) => {
+        const id = Object.keys(it.requirements)[0]
+        const bonus = it.mutations
+            .filter(it => it.mutateAspectId !== "follower_honoured" && it.mutateAspectId !== "resentment")
+            .reduce((acc, it) => {
+                acc[it.mutateAspectId] = it.mutationLevel
+                return acc
+            }, {})
+        acc[id] = {
+            description: it.description,
+            bonus: bonus
+        }
+        return acc
+    }, {})
+
 const tools = readResource("/elements/tools.json").elements
     .filter(it => it.id !== "dropzone")
     .reduce((acc, it) => {
-        acc[it.id] = it
-        delete it.id
+        const tool = {
+            label: it.label,
+            description: it.description,
+            aspects: it.aspects
+        }
+        const gift = toolGifts[it.id]
+        if (gift) {
+            tool.gift = gift
+        }
+        if (it.unique) tool.unique = true
+
+        acc[it.id] = tool
         return acc
     }, {})
 
