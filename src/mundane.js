f886dc8994bc2df3c093fc31ee80104a6e92d7ef
NorthFury <robyter@gmail.com>
2019-04-22 15:40:01 +0200

    trimmed game hints from mundane data

--- src/mundane.js
+++ src/mundane.js
@@ -1,11 +1,12 @@
 const toLuaTable = require("./util/toLuaTable")
 const readResource = require("./util/readResource")
+const trimGameHints = require("./util/trimGameHints")
 
 const mundane = readResource("/elements/mundane_elements.json").elements
     .reduce((acc, it) => {
         acc[it.id] = {
             label: it.label,
-            description: it.description,
+            description: trimGameHints(it.description),
             aspects: it.aspects
         }
         return acc
